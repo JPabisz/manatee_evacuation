5ae40314bc75e59b0d7d9bfac3cd7c0b8fa35d4c
Spencer Hirsch <spencer@yellowcrest.com>
2022-09-07 22:57:51 -0400

    Create main, take input

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/spencerhirsch
+
+go 1.19
---
+++ project01/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Call take input function
+	takeInput()
+}
+
+/*
+Take input from standard input stream and store the data in some container. List? Heap?
+*/
+
+func takeInput() {
+	var numberOfLines int
+	fmt.Scan(&numberOfLines)
+
+	for i := 1; i <= numberOfLines; i++ {
+
+	}
+}
