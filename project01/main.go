0184ca9f74d53ddf62cfcb97db2f3ab6042d2fa9
Spencer Hirsch <spencer@yellowcrest.com>
2022-09-07 23:03:59 -0400

    Added incomplete header

--- project01/main.go
+++ project01/main.go
@@ -1,3 +1,11 @@
+/*
+ * Author: Spencer Hirsch, shirsch2020@my.fit.edu
+ * Author: James Pabisz, jpabisz2020@my.fit.edu
+ * Course: CSE 4250, Fall 2022
+ * Project: project tag, short project name
+ * Implementation: compiler version
+ */
+
 package main
 
 import "fmt"
