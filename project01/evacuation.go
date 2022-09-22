fc6188fa49c108f90b644b1665881b84031ba0e9
JPabisz <64328131+JPabisz@users.noreply.github.com>
2022-09-22 03:04:12 -0400

    Update evacuation.go

--- project01/evacuation.go
+++ project01/evacuation.go
@@ -350,6 +350,52 @@ func occurence_overview(manateeArray []Manatee) [][]Manatee {
 	return manateeRepeatAge
 }
 
+func factorial(n int)(result int) {
+     if (n > 0) {
+         result = n * factorial(n-1)
+         return result
+     }
+     return 1
+}
+
+
+func permutate(sameAge []Manatee) {
+     var n = len(sameAge) - 1
+     var i, j int
+     var searchSpace [][]Manatee
+     var numOfPerms = factorial(len(sameAge))
+     for c := 1; c < numOfPerms; c++ { // 3! = 6:
+            i = n - 1
+            j = n
+            for sameAge[i].number > sameAge[i+1].number {
+                    i--
+            }
+            for sameAge[j].number < sameAge[i].number {
+                    j--
+            }
+            sameAge[i], sameAge[j] = sameAge[j], sameAge[i]
+            j = n
+            i += 1
+            for i < j {
+                    sameAge[i], sameAge[j] = sameAge[j], sameAge[i]
+                    i++
+                    j--
+            }
+            searchSpace = append(searchSpace, sameAge)
+    }
+    possibleTest(searchSpace)
+}
+
+func possibleTest(manatees [][]Manatee) {
+     
+     for _,manatee := range manatees {
+          if isValid(manatee) {
+               output(manatee)
+          }
+     }
+     fmt.Println("impossible")
+}
+
 // Main driver for the program. Calls all necessary functions.
 
 func main() {
